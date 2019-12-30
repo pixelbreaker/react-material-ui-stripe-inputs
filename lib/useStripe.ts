789244281feac72b8dd6fc3032fb5296dd0b7576
Gabriel Mak <pixelbreaker@gmail.com>
2019-12-30 22:00:45 +0000

    initial stripe loader hook

--- lib/useStripe.ts
+++ lib/useStripe.ts
@@ -1,6 +1,43 @@
+import { useEffect, useRef, useState } from 'react'
+
 const useStripe = () => {
-  console.log('useStripe');
-  
+  // guard against SSR
+  if (typeof window === undefined) {
+    return null
+  }
+
+  const loadTimer = useRef<any>(null)
+  const [stripe, setStripe] = useState(null)
+
+  // initiate stripe js
+  if (
+    document.getElementById('stripe-js') !== undefined &&
+    (window as any).Stripe === undefined
+  ) {
+    const script = document.createElement('script')
+    script.async = true
+    script.id = 'stripe-js'
+    script.src = 'https://js.stripe.com/v3/'
+    script.type = 'text/javascript'
+
+    document.getElementsByTagName('head')[0].appendChild(script)
+
+    // check if the stripe scripts are loaded before rendering the form
+    useEffect(() => {
+      loadTimer.current = setInterval(() => {
+        if ((window as any).Stripe !== undefined) {
+          setStripe((window as any).Stripe(process.env.GATSBY_STRIPE_API_KEY))
+          clearInterval(loadTimer.current)
+        }
+      }, 100)
+
+      return () => {
+        clearInterval(loadTimer.current)
+      }
+    }, [])
+  }
+
+  return stripe
 }
 
 export { useStripe }
